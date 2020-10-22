92c377258c8eeab0bb34ed70c41a797b0002096b
wrdias <wanderlei.ribeiro@gmail.com>
2020-10-22 11:28:27 -0300

    criação do projeto cadastro-produtos

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+function App ( props ) {
+  const modificarNome = event => {
+    console.log(event.target.value);
+  }
+
+  const criaComboBox = () =>{
+    const opcoes = [ "Fulano", "Ciclano" ];
+    const comboBoxOptions = opcoes.map(opcao => <option>{opcao}</option>);
+
+    return (
+      <select>
+        {comboBoxOptions}
+      </select>
+    )
+  }
+
+  const MeuComboBox = () => criaComboBox();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <input className = "text-centralizado" type="text" value={props.nome} onChange={modificarNome} />
+    <h1>Hello {props.nome} sua idade é  {props.idade}</h1>
+    <MeuComboBox />
+    </>
+  )
 }
 
 export default App;
